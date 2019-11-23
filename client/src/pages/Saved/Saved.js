63c5b2a2cb8a4137a119988fc58ea2e3aacbb502
kathryn-breslin <kathryn.l.breslin@gmail.com>
2019-11-23 13:57:52 -0500

    rendering saved articles on saved page

--- client/src/pages/Saved/Saved.js
+++ client/src/pages/Saved/Saved.js
@@ -1,16 +1,49 @@
 import React, { Component } from "react";
+import { ArticleCard, Jumbotron, Nav } from "../../components";
 import API from "../../utils/API";
 
 class Saved extends Component {
-    state = {
-        savedBooks: []
-    }
+  state = {
+    savedArticles: []
+  };
 
-    render () {
-        return (
-            <h1>Saved page</h1>
-        )
-    }
+  componentDidMount() {
+    this.getArtNews();
+  }
+
+  getArtNews = () => {
+    API.getArticles().then(
+      res => this.setState({ savedArticles: res.data })
+    );
+  };
+  render() {
+    const { savedArticles } = this.state;
+    return (
+      <div>
+        <Jumbotron />
+        <div className="container">
+          <Nav />
+          <div className="row">
+            {console.log(savedArticles)}
+            {savedArticles.map(saved => {
+              if (saved.saved === true) {
+                return (
+                  <>
+                    <ArticleCard
+                      id={saved._id}
+                      image={saved.image}
+                      title={saved.title}
+                      link={saved.link}
+                    />
+                  </>
+                );
+              }
+            })}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Saved;
